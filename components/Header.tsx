9faca5adbdf47a62d7f6adc8754330a6f9829fd3
Parad0xed <etzhong@usc.edu>
2023-04-23 01:57:17 -0700

    Added temp upload box on dashboard

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import {useRouter} from "next/router"
 import styles from "./Header.module.css";
 
 export function Header(){
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,39 +1,69 @@
 import * as React from "react";
-import { useState } from "react";
-import { Alert, Button, Form, Container, Modal} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.css';
-import styles from "./dashboard.module.css";
+import {useState, useRef, LegacyRef} from "react";
+import { Button, Modal}  from "react-bootstrap";
 import {Header} from "@/components/Header";
 import {DocBlock} from "@/components/DocBlock";
+import 'bootstrap/dist/css/bootstrap.css';
+import styles from "./dashboard.module.css";
 
 const Dashboard = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const handleModal = () => {
+      setShowModal(!showModal);
+  };
   
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+  
+  const fileInputRef = useRef<HTMLInputElement>();
+
+  const handleImageClick = () => {
+    if(fileInputRef.current){
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileUpload = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return; // no file selected
+    if (file.type !== 'application/pdf') {// check that the file is a PDF
+      alert('Please select a PDF file.');
+      return;
+    }
+    setSelectedFile(file);
+    setShowAlert(true);
+
+    // handle file upload logic here (Empty for now)
+    //Fill out primary "Upload" button for future function
+    
+    // reset state after upload
+    setSelectedFile(null);
+    setShowAlert(false);
+  };
+
   return (
     <> 
     <Header/>
     <p className={styles.space}/>
     <div className={styles.div1}>
       <h1 className={styles.myDocuments}>My Documents</h1>
-      <Button variant="primary btn-lg" className={styles.uploadButton}>Upload</Button>
+      <Button variant="primary btn-lg" className={styles.uploadButton} onClick={handleModal}> Upload</Button>
     </div>
-    <div className="modal show" style={{display:'block', position: 'initial'}}>
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <p>Modal body text goes here.</p>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
-    </div>
-
     <p className={styles.space}/>
+
+    <Modal class="modal-dialog modal-lg" show={showModal} onHide={handleModal} className={styles.uploadPromptBox}>
+      <Modal.Body className={styles.uploadPromptBody}>
+        <input type="file" ref={fileInputRef as LegacyRef<HTMLInputElement>} onChange={handleFileUpload} accept=".pdf" style={{ display: 'none' }}/>
+        <label htmlFor="upload" onClick={handleImageClick} style={{cursor: 'pointer'}}>
+          <img src="uploadBox.svg"/>
+        </label>
+      </Modal.Body>
+      <Modal.Footer className={styles.uploadPromptFooter}>
+        <Button variant="secondary" onClick={handleModal}> Close </Button>
+        <Button variant="primary" onClick={handleModal}> Upload </Button>
+      </Modal.Footer>
+    </Modal>
+
     <div className={styles.gridContainer}>
       <DocBlock name="Rigol DG2000 Arbitrary Waveform Generator" pageID="Rigol-DG2000"/>
       <DocBlock name="Rigol DG2000 Arbitrary Waveform Generator" pageID="Rigol-DG2000"/>
