71f9062b507af384dea0e8a54a4303979de9aa3d
Steve Vott <svott@usc.edu>
2023-04-24 09:52:33 -0700

    dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,29 +1,29 @@
 import * as React from "react";
-import {useState, useRef, LegacyRef} from "react";
-import { Button, Modal}  from "react-bootstrap";
-import {Header} from "@/components/Header";
-import {DocBlock} from "@/components/DocBlock";
+import { useState, useRef, LegacyRef } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { Header } from "@/components/Header";
+import { DocBlock } from "@/components/DocBlock";
 import 'bootstrap/dist/css/bootstrap.css';
 import styles from "@/styles/dashboard.module.css";
 
 const Dashboard = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
   const handleModal = () => {
-      setShowModal(!showModal);
+    setShowModal(!showModal);
   };
-  
+
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [showAlert, setShowAlert] = useState<boolean>(false);
-  
+
   const fileInputRef = useRef<HTMLInputElement>();
 
   const handleImageClick = () => {
-    if(fileInputRef.current){
+    if (fileInputRef.current) {
       fileInputRef.current.click();
     }
   };
 
-  const handleFileUpload = (e : React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return; // no file selected
     if (file.type !== 'application/pdf') {// check that the file is a PDF
@@ -35,38 +35,44 @@ const Dashboard = () => {
 
     // handle file upload logic here (Empty for now)
     //Fill out primary "Upload" button for future function
-    
+
     // reset state after upload
     setSelectedFile(null);
     setShowAlert(false);
   };
 
   return (
-    <> 
-    <Header/>
-    <p className={styles.space}/>
-    <div className={styles.div1}>
-      <h1 className={styles.myDocuments}>My Documents</h1>
-      <Button variant="primary btn-lg" className={styles.uploadButton} onClick={handleModal}> Upload</Button>
-    </div>
-    <p className={styles.space}/>
+    <>
+      <Header />
+      <p className={styles.space} />
+      <div className={styles.div1}>
+        <h1 className={styles.myDocuments}>My Documents</h1>
+        <Button variant="primary btn-lg" className={styles.uploadButton} onClick={handleModal}> Upload</Button>
+      </div>
+      <p className={styles.space} />
 
-    <Modal class="modal-dialog modal-lg" show={showModal} onHide={handleModal} className={styles.uploadPromptBox}>
-      <Modal.Body className={styles.uploadPromptBody}>
-        <input type="file" ref={fileInputRef as LegacyRef<HTMLInputElement>} onChange={handleFileUpload} accept=".pdf" style={{ display: 'none' }}/>
-        <label htmlFor="upload" onClick={handleImageClick} style={{cursor: 'pointer'}}>
-          <img src="uploadBox.svg"/>
-        </label>
-      </Modal.Body>
-      <Modal.Footer className={styles.uploadPromptFooter}>
-        <Button variant="secondary" onClick={handleModal}> Close </Button>
-        <Button variant="primary" onClick={handleModal}> Upload </Button>
-      </Modal.Footer>
-    </Modal>
+      <Modal class="modal-dialog modal-lg" show={showModal} onHide={handleModal} className={styles.uploadPromptBox}>
+        <Modal.Body className={styles.uploadPromptBody}>
+          <input type="file" ref={fileInputRef as LegacyRef<HTMLInputElement>} onChange={handleFileUpload} accept=".pdf" style={{ display: 'none' }} />
+          <label htmlFor="upload" onClick={handleImageClick} style={{ cursor: 'pointer' }}>
+            <img src="uploadBox.svg" />
+          </label>
+        </Modal.Body>
+        <Modal.Footer className={styles.uploadPromptFooter}>
+          <Button variant="secondary" onClick={handleModal}> Close </Button>
+          <Button variant="primary" onClick={handleModal}> Upload </Button>
+        </Modal.Footer>
+      </Modal>
 
-    <div className={styles.gridContainer}>
-      <DocBlock name="Rigol DG2000 Arbitrary Waveform Generator" pageID="Rigol-DG2000"/>
-    </div>
+      <div className={styles.gridContainer}>
+        <DocBlock name="Rigol DG2000 Arbitrary Waveform Generator" pageID="Rigol-DG2000" documentName="Rigol-DG2000" />
+        <DocBlock name="FPGA" pageID="FPGA" documentName="FPGA" />
+        <DocBlock name="Guitar" pageID="Guitar" />
+        <DocBlock name="McDonalds Ice Cream Machine" pageID="McDonalds" documentName="McDonalds" />
+        <DocBlock name="LG Washing Machine" pageID="Washing" documentName="Washing" />
+        <DocBlock name="Oscilloscope" pageID="Oscilloscope" documentName="Oscilloscope" />
+        <DocBlock name="Atmega328P" pageID="Atmega" documentName="Atmega" />
+      </div>
 
     </>
   );
